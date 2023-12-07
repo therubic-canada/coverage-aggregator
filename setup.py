885d687f853e955ff9b54dadc78ac0d3dc316093
augustelalande <auguste.lalande@gmail.com>
2023-12-06 21:31:44 -0500

    generate badge.json instead of badge.svg

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'coverage-aggregator'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     description='Coverage report aggregator',
     packages=['coverage_aggregator'],
     install_requires=['setuptools', 'jinja2'],
